a9ca3c68c29f45292621cae9d14989316cccfeab
devsaurus <devsaurus@users.noreply.github.com>
2004-03-23 21:24:33 +0000

    initial check-in
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@2 4b6b2881-338b-4f6d-858e-48f7cbb1be58

---
+++ t48_tb_pack-p.vhd
@@ -0,0 +1,19 @@
+-------------------------------------------------------------------------------
+--
+-- $Id: t48_tb_pack-p.vhd,v 1.1 2004-03-23 21:24:33 arniml Exp $
+--
+-- Copyright (c) 2004, Arnim Laeuger (arniml@opencores.org)
+--
+-- All rights reserved
+--
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package t48_tb_pack is
+
+  -- Accumulator visibilty
+  signal tb_accu_s : std_logic_vector(7 downto 0);
+
+end t48_tb_pack;
