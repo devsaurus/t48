91ed0508fc734902260efdb0a8875843f18bac88
devsaurus <devsaurus@users.noreply.github.com>
2004-04-16 22:17:08 +0000

    expand script for dump compare
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@61 4b6b2881-338b-4f6d-858e-48f7cbb1be58

--- sw/run_regression.pl
+++ sw/run_regression.pl
@@ -4,7 +4,7 @@
 #
 # run_regression.pl
 #
-# $Id: run_regression.pl,v 1.2 2004-04-09 19:17:09 arniml Exp $
+# $Id: run_regression.pl,v 1.3 2004-04-16 22:17:08 arniml Exp $
 #
 # Copyright (c) 2004, Arnim Laeuger (arniml@opencores.org)
 #
@@ -17,6 +17,9 @@
 #
 # Runs regression suite over all testcells found in $VERIF_DIR.
 #
+# run_regression.pl [-d]
+#  -d : Perform a dump compare on each test with the i8039 simulator.
+#
 # The testcells are identified by searching for the .asm file(s).
 # Each testcell is built by calling the central Makefile.cell.
 # The resulting hex-file is then copied to $SIM_DIR where the VHDL simulator
@@ -26,22 +29,49 @@
 
 use strict;
 
+use Getopt::Std;
+
+
+sub print_usage {
+    print <<EOU;
+Runs regression tests in \$VERIF_DIR.
+Usage:
+ run_regression.pl [-d]
+  -d : Perform a dump compare on each test with the i8039 simulator.
+EOU
+}
+
+
+my %options;
 my (@asm_files, $asm_file);
 my (%cells, $cell, $cell_dir, $tag);
 my $pwd;
+my $dump_compare = 0;
 
 
 ##############################################################################
 # Commands to call the different VHDL simulators.
 # 
 # GHDL
-my $ghdl_simulator = './tb_behav_c0';
+my $ghdl_simulator     = './tb_behav_c0';
+my $ghdl_simulator_vcd = $ghdl_simulator.' --vcd=temp.vcd';
 #
 # Choose simulator:
-my $vhdl_simulator = $ghdl_simulator;
+my $vhdl_simulator     = $ghdl_simulator;
+my $vhdl_simulator_vcd = $ghdl_simulator_vcd;
 #
 ##############################################################################
 
+
+# process command line options
+if (!getopts('d', \%options)) {
+    print_usage();
+    exit(1);
+}
+
+if (exists($options{'d'})) {
+    $dump_compare = 1;
+}
 
 $pwd = `pwd`;
 chomp($pwd);
@@ -70,7 +100,24 @@ while (($cell, $tag) = each(%cells)) {
         if ($? == 0) {
             chdir($ENV{'SIM_DIR'});
             system('sh', '-c', 'ls -l t48_rom.hex');
-            system('sh', '-c', $vhdl_simulator);
+            system('sh', '-c', $dump_compare > 0 ? $vhdl_simulator_vcd : $vhdl_simulator);
+
+            if ($dump_compare) {
+                system('sh', '-c', 'rm -f dump sim.dump vhdl.dump');
+                system('sh', '-c',
+                       'vcd2vec.pl -s ../../sw/dump_compare.signals < temp.vcd | vec2dump.pl > vhdl.dump');
+                system('sh', '-c', 'i8039 -f t48_rom.hex -d > dump');
+                system('sh', '-c', 'egrep \':.+\|\' dump | sed -e \'s/[^|]*. *//\' > sim.dump');
+                system('sh', '-c', 'diff -b -q sim.dump vhdl.dump');
+                print("Dump Compare: ");
+                if ($? == 0) {
+                    print("PASS\n");
+                } else {
+                    print("FAIL\n");
+                }
+                system('sh', '-c', 'rm -f dump sim.dump vhdl.dump temp.vcd');
+            }
+
         } else {
             print("Error: Cannot make cell $cell!\n");
         }
