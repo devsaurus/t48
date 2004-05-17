bfdb1bfa9c057863c8f6a043472859721784797b
devsaurus <devsaurus@users.noreply.github.com>
2004-05-17 14:42:09 +0000

    update for changes in address space of external memory
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@102 4b6b2881-338b-4f6d-858e-48f7cbb1be58

--- sw/verif/black_box/movx/test.asm
+++ sw/verif/black_box/movx/test.asm
@@ -1,5 +1,5 @@
 	;; *******************************************************************
-	;; $Id: test.asm,v 1.1.1.1 2004-03-25 22:29:18 arniml Exp $
+	;; $Id: test.asm,v 1.2 2004-05-17 14:42:09 arniml Exp $
 	;;
 	;; Test MOVX A, @ Rr for RB0.
 	;; *******************************************************************
@@ -10,7 +10,7 @@
 	ORG	0
 
 	;; Start of test
-	mov	r0, #0FFH
+	mov	r0, #0FEH
 fill_loop1:
 	mov	a, r0
 	movx	@r0, a
@@ -19,35 +19,41 @@ fill_loop1:
 	;; check memory
 	mov	a, r0
 	jnz	fail
-	mov	r0, #0FFH
-	mov	r1, #001H
+	mov	r0, #0FEH
+	mov	r1, #002H
 check_loop1:
+	clr	a
 	movx	a, @r1
 	add	a, r0
 	jnz	fail
-	dec	r0
 	inc	r1
-	mov	a, r1
+	dec	r0
+	mov	a, r0
+	dec	a
 	jnz	check_loop1
 
 
 
-	mov	r1, #0FFH
-	mov	a, #001H
+	mov	r1, #0FEH
+	mov	a, #002H
 fill_loop2:
 	movx	@r1, a
 	inc	a
 	djnz	r1, fill_loop2
 
 	;; check memory
-	mov	a, r0
+	mov	a, r1
 	jnz	fail
-	mov	r0, #0FFH
+	mov	r0, #0FEH
 check_loop2:
+	clr	a
 	movx	a, @r0
 	add	a, r0
 	jnz	fail
-	djnz	r0, check_loop2
+	dec	r0
+	mov	a, r0
+	dec	a
+	jnz	check_loop2
 
 pass:	PASS
 
