2a2c20522aaf1e094c1f51405275f1d63d2268ff
devsaurus <devsaurus@users.noreply.github.com>
2004-05-21 11:22:44 +0000

    exchange syn_rom for lpm_rom
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@110 4b6b2881-338b-4f6d-858e-48f7cbb1be58

--- bench/vhdl/tb-c.vhd
+++ bench/vhdl/tb-c.vhd
@@ -2,7 +2,7 @@
 --
 -- The testbench for t48_core.
 --
--- $Id: tb-c.vhd,v 1.2 2004-04-25 16:23:21 arniml Exp $
+-- $Id: tb-c.vhd,v 1.3 2004-05-21 11:22:44 arniml Exp $
 --
 -- Copyright (c) 2004, Arnim Laeuger (arniml@opencores.org)
 --
@@ -14,8 +14,12 @@ configuration tb_behav_c0 of tb is
 
   for behav
 
-    for rom_4k : syn_rom
-      use configuration work.syn_rom_lpm_c0;
+    for rom_internal_2k : lpm_rom
+      use configuration work.lpm_rom_c0;
+    end for;
+
+    for rom_external_2k : lpm_rom
+      use configuration work.lpm_rom_c0;
     end for;
 
     for ram_256 : syn_ram
@@ -43,6 +47,9 @@ end tb_behav_c0;
 -- File History:
 --
 -- $Log: not supported by cvs2svn $
+-- Revision 1.2  2004/04/25 16:23:21  arniml
+-- added if_timing
+--
 -- Revision 1.1  2004/03/24 21:42:10  arniml
 -- initial check-in
 --
