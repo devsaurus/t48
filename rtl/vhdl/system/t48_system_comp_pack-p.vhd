4d573e331f1e444deb88a7c3f142b3c4325f22cf
devsaurus <devsaurus@users.noreply.github.com>
2005-05-05 19:49:58 +0000

    add component wb_master.vhd
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@165 4b6b2881-338b-4f6d-858e-48f7cbb1be58

--- rtl/vhdl/system/t48_system_comp_pack-p.vhd
+++ rtl/vhdl/system/t48_system_comp_pack-p.vhd
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 --
--- $Id: t48_system_comp_pack-p.vhd,v 1.3 2004-12-03 19:43:12 arniml Exp $
+-- $Id: t48_system_comp_pack-p.vhd,v 1.4 2005-05-05 19:49:58 arniml Exp $
 --
 -- Copyright (c) 2004, Arnim Laeuger (arniml@opencores.org)
 --
@@ -12,6 +12,27 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package t48_system_comp_pack is
+
+  component wb_master
+    port (
+      xtal_i      : in  std_logic;
+      res_i       : in  std_logic;
+      en_clk_o    : out std_logic;
+      ale_i       : in  std_logic;
+      rd_n_i      : in  std_logic;
+      wr_n_i      : in  std_logic;
+      sel_range_i : in  std_logic_vector( 1 downto 0);
+      db_bus_i    : in  std_logic_vector( 7 downto 0);
+      db_bus_o    : out std_logic_vector( 7 downto 0);
+      wb_cyc_o    : out std_logic;
+      wb_stb_o    : out std_logic;
+      wb_we_o     : out std_logic;
+      wb_adr_o    : out std_logic_vector(23 downto 0);
+      wb_ack_i    : in  std_logic;
+      wb_dat_i    : in  std_logic_vector( 7 downto 0);
+      wb_dat_o    : out std_logic_vector( 7 downto 0)
+    );
+  end component;
 
   component t8048_notri
     generic (
@@ -72,6 +93,41 @@ package t48_system_comp_pack is
       p1_o         : out std_logic_vector( 7 downto 0);
       p1_low_imp_o : out std_logic;
       prog_n_o     : out std_logic
+    );
+  end component;
+
+  component t8050_wb
+    generic (
+      gate_port_input_g : integer := 1
+    );
+
+    port (
+      xtal_i       : in  std_logic;
+      reset_n_i    : in  std_logic;
+      t0_i         : in  std_logic;
+      t0_o         : out std_logic;
+      t0_dir_o     : out std_logic;
+      int_n_i      : in  std_logic;
+      ea_i         : in  std_logic;
+      rd_n_o       : out std_logic;
+      psen_n_o     : out std_logic;
+      wr_n_o       : out std_logic;
+      ale_o        : out std_logic;
+      t1_i         : in  std_logic;
+      p2_i         : in  std_logic_vector( 7 downto 0);
+      p2_o         : out std_logic_vector( 7 downto 0);
+      p2_low_imp_o : out std_logic;
+      p1_i         : in  std_logic_vector( 7 downto 0);
+      p1_o         : out std_logic_vector( 7 downto 0);
+      p1_low_imp_o : out std_logic;
+      prog_n_o     : out std_logic;
+      wb_cyc_o     : out std_logic;
+      wb_stb_o     : out std_logic;
+      wb_we_o      : out std_logic;
+      wb_adr_o     : out std_logic_vector(23 downto 0);
+      wb_ack_i     : in  std_logic;
+      wb_dat_i     : in  std_logic_vector( 7 downto 0);
+      wb_dat_o     : out std_logic_vector( 7 downto 0)
     );
   end component;
 
