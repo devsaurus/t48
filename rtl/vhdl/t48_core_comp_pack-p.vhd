8b9db4ab345d03dab239574c6802592850cb5a21
devsaurus <devsaurus@users.noreply.github.com>
2005-11-01 21:33:41 +0000

    entity changes for P2 low impedance issue
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@209 4b6b2881-338b-4f6d-858e-48f7cbb1be58

--- rtl/vhdl/t48_core_comp_pack-p.vhd
+++ rtl/vhdl/t48_core_comp_pack-p.vhd
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 --
--- $Id: t48_core_comp_pack-p.vhd,v 1.2 2004-03-29 19:39:58 arniml Exp $
+-- $Id: t48_core_comp_pack-p.vhd,v 1.3 2005-11-01 21:33:41 arniml Exp $
 --
 -- Copyright (c) 2004, Arnim Laeuger (arniml@opencores.org)
 --
@@ -25,37 +25,38 @@ package t48_core_comp_pack is
     );
 
     port (
-      xtal_i       : in  std_logic;
-      reset_i      : in  std_logic;
-      t0_i         : in  std_logic;
-      t0_o         : out std_logic;
-      t0_dir_o     : out std_logic;
-      int_n_i      : in  std_logic;
-      ea_i         : in  std_logic;
-      rd_n_o       : out std_logic;
-      psen_n_o     : out std_logic;
-      wr_n_o       : out std_logic;
-      ale_o        : out std_logic;
-      db_i         : in  std_logic_vector( 7 downto 0);
-      db_o         : out std_logic_vector( 7 downto 0);
-      db_dir_o     : out std_logic;
-      t1_i         : in  std_logic;
-      p2_i         : in  std_logic_vector( 7 downto 0);
-      p2_o         : out std_logic_vector( 7 downto 0);
-      p2_low_imp_o : out std_logic;
-      p1_i         : in  std_logic_vector( 7 downto 0);
-      p1_o         : out std_logic_vector( 7 downto 0);
-      p1_low_imp_o : out std_logic;
-      prog_n_o     : out std_logic;
-      clk_i        : in  std_logic;
-      en_clk_i     : in  std_logic;
-      xtal3_o      : out std_logic;
-      dmem_addr_o  : out std_logic_vector( 7 downto 0);
-      dmem_we_o    : out std_logic;
-      dmem_data_i  : in  std_logic_vector( 7 downto 0);
-      dmem_data_o  : out std_logic_vector( 7 downto 0);
-      pmem_addr_o  : out std_logic_vector(11 downto 0);
-      pmem_data_i  : in  std_logic_vector( 7 downto 0)
+      xtal_i        : in  std_logic;
+      reset_i       : in  std_logic;
+      t0_i          : in  std_logic;
+      t0_o          : out std_logic;
+      t0_dir_o      : out std_logic;
+      int_n_i       : in  std_logic;
+      ea_i          : in  std_logic;
+      rd_n_o        : out std_logic;
+      psen_n_o      : out std_logic;
+      wr_n_o        : out std_logic;
+      ale_o         : out std_logic;
+      db_i          : in  std_logic_vector( 7 downto 0);
+      db_o          : out std_logic_vector( 7 downto 0);
+      db_dir_o      : out std_logic;
+      t1_i          : in  std_logic;
+      p2_i          : in  std_logic_vector( 7 downto 0);
+      p2_o          : out std_logic_vector( 7 downto 0);
+      p2l_low_imp_o : out std_logic;
+      p2h_low_imp_o : out std_logic;
+      p1_i          : in  std_logic_vector( 7 downto 0);
+      p1_o          : out std_logic_vector( 7 downto 0);
+      p1_low_imp_o  : out std_logic;
+      prog_n_o      : out std_logic;
+      clk_i         : in  std_logic;
+      en_clk_i      : in  std_logic;
+      xtal3_o       : out std_logic;
+      dmem_addr_o   : out std_logic_vector( 7 downto 0);
+      dmem_we_o     : out std_logic;
+      dmem_data_i   : in  std_logic_vector( 7 downto 0);
+      dmem_data_o   : out std_logic_vector( 7 downto 0);
+      pmem_addr_o   : out std_logic_vector(11 downto 0);
+      pmem_data_i   : in  std_logic_vector( 7 downto 0)
     );
   end component;
 
