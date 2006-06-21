d8b9bc92bb6b40d7dc1aace3a949036fcdd6cc54
devsaurus <devsaurus@users.noreply.github.com>
2006-06-21 01:03:28 +0000

    replaced syn_ram and syn_rom with generic_ram_ena and t48_rom/t49_rom
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@227 4b6b2881-338b-4f6d-858e-48f7cbb1be58

--- rtl/vhdl/t48_core_comp_pack-p.vhd
+++ rtl/vhdl/t48_core_comp_pack-p.vhd
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 --
--- $Id: t48_core_comp_pack-p.vhd,v 1.4 2006-06-20 00:46:04 arniml Exp $
+-- $Id: t48_core_comp_pack-p.vhd,v 1.5 2006-06-21 01:03:28 arniml Exp $
 --
 -- Copyright (c) 2004, Arnim Laeuger (arniml@opencores.org)
 --
@@ -61,28 +61,34 @@ package t48_core_comp_pack is
     );
   end component;
 
-  component syn_rom
+  component generic_ram_ena
     generic (
-      address_width_g : positive := 10
+      addr_width_g : integer := 10;
+      data_width_g : integer := 8
     );
     port (
+      clk_i : in  std_logic;
+      a_i   : in  std_logic_vector(addr_width_g-1 downto 0);
+      we_i  : in  std_logic;
+      ena_i : in  std_logic;
+      d_i   : in  std_logic_vector(data_width_g-1 downto 0);
+      d_o   : out std_logic_vector(data_width_g-1 downto 0)
+    );
+  end component;
+
+  component t48_rom
+    port (
       clk_i      : in  std_logic;
-      rom_addr_i : in  std_logic_vector(address_width_g-1 downto 0);
+      rom_addr_i : in  std_logic_vector(9 downto 0);
       rom_data_o : out std_logic_vector(7 downto 0)
     );
   end component;
 
-  component syn_ram
-    generic (
-      address_width_g : positive := 8
-    );
+  component t49_rom
     port (
       clk_i      : in  std_logic;
-      res_i      : in  std_logic;
-      ram_addr_i : in  std_logic_vector(address_width_g-1 downto 0);
-      ram_data_i : in  std_logic_vector(7 downto 0);
-      ram_we_i   : in  std_logic;
-      ram_data_o : out std_logic_vector(7 downto 0)
+      rom_addr_i : in  std_logic_vector(10 downto 0);
+      rom_data_o : out std_logic_vector( 7 downto 0)
     );
   end component;
 
