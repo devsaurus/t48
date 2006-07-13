71e115a977ac8085d430c16f78f7a18d369a9230
devsaurus <devsaurus@users.noreply.github.com>
2006-07-13 22:55:10 +0000

    initial check-in
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@247 4b6b2881-338b-4f6d-858e-48f7cbb1be58

---
+++ bench/vhdl/tb_t8048_t8243-c.vhd
@@ -0,0 +1,42 @@
+-------------------------------------------------------------------------------
+--
+-- The testbench for t8048 driving a t8243.
+--
+-- $Id: tb_t8048_t8243-c.vhd,v 1.1 2006-07-13 22:55:10 arniml Exp $
+--
+-- Copyright (c) 2006, Arnim Laeuger (arniml@opencores.org)
+--
+-- All rights reserved
+--
+-------------------------------------------------------------------------------
+
+configuration tb_t8048_t8243_behav_c0 of tb_t8048_t8243 is
+
+  for behav
+
+    for ext_ram_b : generic_ram_ena
+      use configuration work.generic_ram_ena_rtl_c0;
+    end for;
+
+    for ext_rom_b : lpm_rom
+      use configuration work.lpm_rom_c0;
+    end for;
+
+    for t8048_b : t8048
+      use configuration work.t8048_struct_c0;
+    end for;
+
+    for t8243_b : t8243
+      use configuration work.t8243_struct_c0;
+    end for;
+
+  end for;
+
+end tb_t8048_t8243_behav_c0;
+
+
+-------------------------------------------------------------------------------
+-- File History:
+--
+-- $Log: not supported by cvs2svn $
+-------------------------------------------------------------------------------
---
+++ rtl/vhdl/t8243/t8243_async_notri-c.vhd
@@ -0,0 +1,19 @@
+-------------------------------------------------------------------------------
+--
+-- T8243 Core
+--
+-- $Id: t8243_async_notri-c.vhd,v 1.1 2006-07-13 22:53:56 arniml Exp $
+--
+-------------------------------------------------------------------------------
+
+configuration t8243_async_notri_struct_c0 of t8243_async_notri is
+
+  for struct
+
+    for t8243_core_b: t8243_core
+      use configuration work.t8243_core_rtl_c0;
+    end for;
+
+  end for;
+
+end t8243_async_notri_struct_c0;
