c1c1ee67fc69f5bd0072484a79ec1b6b87e204ad
devsaurus <devsaurus@users.noreply.github.com>
2006-07-14 01:02:47 +0000

    initial check-in
    git-svn-id: https://opencores.org/ocsvn/t48/trunk@248 4b6b2881-338b-4f6d-858e-48f7cbb1be58

---
+++ bench/vhdl/tb_t8243-c.vhd
@@ -0,0 +1,54 @@
+-------------------------------------------------------------------------------
+--
+-- The testbench for t8243 core.
+--
+-- $Id: tb_t8243-c.vhd,v 1.1 2006-07-14 01:02:47 arniml Exp $
+--
+-- Copyright (c) 2006, Arnim Laeuger (arniml@opencores.org)
+--
+-- All rights reserved
+--
+-------------------------------------------------------------------------------
+
+configuration tb_t8243_behav_c0 of tb_t8243 is
+
+  for behav
+
+    for rom_internal_2k : lpm_rom
+      use configuration work.lpm_rom_c0;
+    end for;
+
+    for rom_external_2k : lpm_rom
+      use configuration work.lpm_rom_c0;
+    end for;
+
+    for ram_256 : generic_ram_ena
+      use configuration work.generic_ram_ena_rtl_c0;
+    end for;
+
+    for ext_ram_b : generic_ram_ena
+      use configuration work.generic_ram_ena_rtl_c0;
+    end for;
+
+    for t48_core_b : t48_core
+      use configuration work.t48_core_struct_c0;
+    end for;
+
+    for t8243_sync_notri_b : t8243_sync_notri
+      use configuration work.t8243_sync_notri_struct_c0;
+    end for;
+
+    for if_timing_b : if_timing
+      use configuration work.if_timing_behav_c0;
+    end for;
+
+  end for;
+
+end tb_t8243_behav_c0;
+
+
+-------------------------------------------------------------------------------
+-- File History:
+--
+-- $Log: not supported by cvs2svn $
+-------------------------------------------------------------------------------
