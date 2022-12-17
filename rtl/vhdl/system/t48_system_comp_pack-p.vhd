392161ff0084f847c30a70ab8e8e61d3ed2af538
devsaurus <devsaurus@users.noreply.github.com>
2022-12-17 20:55:34 +0000

    add t8041
    git-svn-id: https://opencores.org/ocsvn/t48/t48/trunk@305 4b6b2881-338b-4f6d-858e-48f7cbb1be58

--- rtl/vhdl/system/t48_system_comp_pack-p.vhd
+++ rtl/vhdl/system/t48_system_comp_pack-p.vhd
@@ -174,6 +174,54 @@ package t48_system_comp_pack is
     );
   end component;
 
+  component t8041_notri
+    generic (
+      gate_port_input_g : integer := 1
+    );
+
+    port (
+      xtal_i        : in  std_logic;
+      xtal_en_i     : in  std_logic;
+      reset_n_i     : in  std_logic;
+      t0_i          : in  std_logic;
+      cs_n_i        : in  std_logic;
+      rd_n_i        : in  std_logic;
+      a0_i          : in  std_logic;
+      wr_n_i        : in  std_logic;
+      sync_o        : out std_logic;
+      db_i          : in  std_logic_vector( 7 downto 0);
+      db_o          : out std_logic_vector( 7 downto 0);
+      db_dir_o      : out std_logic;
+      t1_i          : in  std_logic;
+      p2_i          : in  std_logic_vector( 7 downto 0);
+      p2_o          : out std_logic_vector( 7 downto 0);
+      p2l_low_imp_o : out std_logic;
+      p2h_low_imp_o : out std_logic;
+      p1_i          : in  std_logic_vector( 7 downto 0);
+      p1_o          : out std_logic_vector( 7 downto 0);
+      p1_low_imp_o  : out std_logic;
+      prog_n_o      : out std_logic
+    );
+  end component;
+
+  component t8041
+    port (
+      xtal_i    : in    std_logic;
+      reset_n_i : in    std_logic;
+      t0_i      : in    std_logic;
+      cs_n_i    : in    std_logic;
+      rd_n_i    : in    std_logic;
+      a0_i      : in    std_logic;
+      wr_n_i    : in    std_logic;
+      sync_o    : out   std_logic;
+      db_b      : inout std_logic_vector( 7 downto 0);
+      t1_i      : in    std_logic;
+      p2_b      : inout std_logic_vector( 7 downto 0);
+      p1_b      : inout std_logic_vector( 7 downto 0);
+      prog_n_o  : out   std_logic
+    );
+  end component;
+
   component t8041a_notri
     generic (
       gate_port_input_g : integer := 1
