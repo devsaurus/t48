4dd3814f0d4a6d61aa3ed6dc07049c384013ce6d
devsaurus <devsaurus@users.noreply.github.com>
2022-12-25 20:53:05 +0000

    improve in test
    git-svn-id: https://opencores.org/ocsvn/t48/t48/trunk@320 4b6b2881-338b-4f6d-858e-48f7cbb1be58

--- sw/verif/black_box/in/test.asm
+++ sw/verif/black_box/in/test.asm
@@ -17,12 +17,27 @@
 
 	clr	a
 	in	a, p1
-	add	a, #0ABH
+	xrl	a, #055H
 	jnz	fail
 
 	in	a, p2
-	add	a, #056H
+	xrl	a, #0AAH
 	jnz	fail
+
+	mov	a, #0AAH
+	outl	p1, a
+	clr	a
+	mov	a, #055H
+	outl	p2, a
+
+	clr	a
+	in	a, p1
+	xrl	a, #0AAH
+	jnz	fail
+
+	;in	a, p2
+	;xrl	a, #055H
+	;jnz	fail
 
 pass:	PASS
 
